5a521b78abc882c06dc3f6a95a9517dd61e89851
Alexey-Kapitonov <dekaliostro@gmail.com>
2021-03-16 18:58:31 +0200

    task-8-gallery

--- main-gallery.js
+++ main-gallery.js
@@ -1,3 +1,28 @@
 import images from './gallery-items.js'
 
-//console.log(images);
+//Создание и рендер разметки по массиву данных и предоставленному шаблону.
+
+const galleryUlRef = document.querySelector('.js-gallery');
+
+
+const imageCardMarkup = images.map(({ preview, original, description }) => {
+    
+    return `<li class="gallery__item">
+<a
+    class="gallery__link"
+    href="${original}"
+  >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+       
+});
+
+galleryUlRef.insertAdjacentHTML('afterbegin', imageCardMarkup.join(''))
+
+
