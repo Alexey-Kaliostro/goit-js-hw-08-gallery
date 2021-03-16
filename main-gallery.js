25efa7268607ee5252557865ef86b7b99cbe3beb
Alexey-Kapitonov <dekaliostro@gmail.com>
2021-03-16 19:46:45 +0200

    gallery-update

--- main-gallery.js
+++ main-gallery.js
@@ -2,9 +2,10 @@ import images from './gallery-items.js'
 
 //Создание и рендер разметки по массиву данных и предоставленному шаблону.
 
+// Получение DOM ссылки на <ul> галереи
 const galleryUlRef = document.querySelector('.js-gallery');
 
-
+// создание разметки карточки изображения
 const imageCardMarkup = images.map(({ preview, original, description }) => {
     
     return `<li class="gallery__item">
@@ -23,6 +24,8 @@ const imageCardMarkup = images.map(({ preview, original, description }) => {
        
 });
 
+// генерация разметки всей галереии 
 galleryUlRef.insertAdjacentHTML('afterbegin', imageCardMarkup.join(''))
 
+// Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
 
